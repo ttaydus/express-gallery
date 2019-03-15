2a79180e5144a5870297a8cfcab4fd8291a019dd
Tyler <tylertaydus4444@gmail.com>
2019-03-14 19:29:29 -1000

    server.js up to date

--- server.js
+++ server.js
@@ -1,2 +1,53 @@
-//tester this biggabooooooommm!!!!!!!
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('./database/models/User');
+
+// data vars
+const PORT = process.env.PORT;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
+
+if (!PORT) { console.log('No Port Found'); }
+if (!SESSION_SECRET) { console.log('No Session Secret Found'); }
+if (!REDIS_HOSTNAME) { console.log('No Redis Hostname Found'); }
+if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) { return process.exit(1); }
+
+// setup server middleware
+const app = express();
+app.use(bodyParser.json({ extended: true }));
+
+// routes
+app.get('/api/smoke', (req, res) => {
+  res.json({ smoke: 'test' });
+});
+
+app.get('/api/users', (req, res) => {
+  return new User().fetchAll()
+    .then((users) => {
+      return res.json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  return new User({ username }).save()
+    .then((user) => {
+      return res.json({ success: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+})
+
+//console.log(process.env)
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server stated on port: ${PORT}`);
+});
 
